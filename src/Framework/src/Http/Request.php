49d03ecf6d2a424221b3874b3283c77979f43a35
Raphaël Geffroy <81738559+raphael-geffroy@users.noreply.github.com>
2022-02-09 11:03:43 +0100

    request & response

---
+++ src/Framework/src/Http/Request.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\Http;
+
+final class Request
+{
+    function __construct(
+        private string $pathInfo = "/",
+        private array $params = []
+    ){}
+
+    /**
+     * @return string
+     */
+    public function getPathInfo(): string
+    {
+        return $this->pathInfo;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    /**
+     * @param string $key
+     * @return string|null
+     */
+    public function getParam(string $key): ?string
+    {
+        return $this->params[$key] ?? null;
+    }
+
+
+}
